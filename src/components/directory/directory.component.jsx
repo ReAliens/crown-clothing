458ff7ec5db3ba45c2fd056df804ffacfafd1c8b
ahmed <ahmedreda152@gmail.com>
2020-03-19 21:53:43 +0200

    updating our app with shop page,collection preview and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -34,14 +34,14 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/womens'
+                    linkUrl: 'shop/women'
                 },
                 {
-                    title: 'mens',
+                    title: 'men',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/mens'
+                    linkUrl: 'shop/men'
                 }
             ]
         }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import SHOP_DATA from './shop.data.js';
+import CollectionPreview from '../../components/preview-collection/collection-preview';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
+    render() {
+        const {collections}= this.state;
+        return (<div className='shop-page'>
+            {
+            collections.map(({ id , ...otherCollectionProps}) => (
+                    <CollectionPreview key={id}{...otherCollectionProps}/>
+                ))
+            }
+        </div>);
+
+    }
+}
+
+export default ShopPage;
