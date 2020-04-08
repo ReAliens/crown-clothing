ea907dbfea4402a16d04c11230b435ad72c147e0
ahmed <ahmedreda152@gmail.com>
2020-04-08 08:42:14 +0200

    Updates

---
+++ precache-manifest.328ae9ed9ea7454aa79e45e722878538.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9c76c7bca9818b867b9a12830e8084",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "3b38eb04f83c46702752",
+    "url": "/crown-clothing/static/css/main.df0e360c.chunk.css"
+  },
+  {
+    "revision": "7e42e5835968836b848d",
+    "url": "/crown-clothing/static/js/2.454a0cec.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/crown-clothing/static/js/2.454a0cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b38eb04f83c46702752",
+    "url": "/crown-clothing/static/js/main.d341e467.chunk.js"
+  },
+  {
+    "revision": "7f96ec3a1bff7dc5f163",
+    "url": "/crown-clothing/static/js/runtime-main.76b8c2eb.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
