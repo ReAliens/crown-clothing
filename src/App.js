734fef571e40c9b8aaf887b532785ad0ef3468ce
Ahmed <ahmedreda152@gmail.com>
2021-10-01 22:41:35 +0200

    edit paths

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/crown-clothing' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
+          <Route path='/crown-clothing/shop' component={ShopPage} />
+          <Route exact path='/crown-clothing/checkout' component={CheckoutPage} />
+          <Route exact path='/crown-clothing/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
         </Switch>
       </div>
     );
