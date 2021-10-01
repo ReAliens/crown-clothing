76cbf4055da415721db0a1076b8526617c396da4
Ahmed <ahmedreda152@gmail.com>
2021-10-01 19:13:20 +0200

    build

---
+++ build/precache-manifest.971a5e3aa23714e69ef9efcb09ec1894.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a483593641265d941977e728e9eff5f",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "2a85a730fb6d40efeadc",
+    "url": "/crown-clothing/static/css/main.49930d92.chunk.css"
+  },
+  {
+    "revision": "9518e4de017224b6fa2b",
+    "url": "/crown-clothing/static/js/2.b0f875a4.chunk.js"
+  },
+  {
+    "revision": "228171506cbce993089d799cbe59e7ff",
+    "url": "/crown-clothing/static/js/2.b0f875a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a85a730fb6d40efeadc",
+    "url": "/crown-clothing/static/js/main.00e4ddb6.chunk.js"
+  },
+  {
+    "revision": "7f96ec3a1bff7dc5f163",
+    "url": "/crown-clothing/static/js/runtime-main.76b8c2eb.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crown-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/crown-clothing/precache-manifest.971a5e3aa23714e69ef9efcb09ec1894.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crown-clothing/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
